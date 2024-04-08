22d31c756ef8c2bbdce4078e88d64c40bb9912e6
Avishek <pradhan.avishek1994@gmail.com>
2024-04-08 23:19:02 +0530

    Avishek First Commit

---
+++ src/Test/Avis1.java
@@ -0,0 +1,15 @@
+package Test;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Avis1 {
+	 String a = "chrome";
+	@Test
+ public void print() {
+	if(a.) {
+		 System.out.println("Avishek");
+	}
+	 System.out.println("Not present");
+ }
+}
---
+++ src/Test/Vv.java
@@ -0,0 +1,15 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Vv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println(System.getProperty("user.dir"));
+      
+	}
+}
