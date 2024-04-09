f325e10afd0fa2574857cf8bfc7dc122671e8666
Avishek <pradhan.avishek1994@gmail.com>
2024-04-09 11:33:06 +0530

    My New commit

--- src/Test/Avis1.java
+++ src/Test/Avis1.java
@@ -7,7 +7,7 @@ public class Avis1 {
 	 String a = "chrome";
 	@Test
  public void print() {
-	if(a.) {
+	if(a.contains("chr")) {
 		 System.out.println("Avishek");
 	}
 	 System.out.println("Not present");
